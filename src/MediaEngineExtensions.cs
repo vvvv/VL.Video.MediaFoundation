1a6d0ffdf5b1bc1786c6e69b7a091c958be39046
Elias Holzer <elias@vvvv.org>
2020-04-10 22:50:47 +0200

    Added VideoPlayer and VideoPlayer (Reactive) nodes

---
+++ src/MediaEngineExtensions.cs
@@ -0,0 +1,71 @@
+﻿using SharpDX.MediaFoundation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VL.MediaFoundation
+{
+    static class MediaEngineExtensions
+    {
+        public static async Task LoadAsync(this MediaEngine engine, string url, CancellationToken token)
+        {
+            await engine.DoAsync(token, e => e == MediaEngineEvent.LoadedMetadata, () =>
+            {
+                engine.Source = url;
+            });
+        }
+
+        public static async Task PlayAsync(this MediaEngine engine, CancellationToken token)
+        {
+            await engine.DoAsync(token, e => e == MediaEngineEvent.Playing || e == MediaEngineEvent.TimeUpdate, () => engine.Play());
+        }
+
+        public static async Task PauseAsync(this MediaEngine engine, CancellationToken token)
+        {
+            await engine.DoAsync(token, e => e == MediaEngineEvent.Pause, () => engine.Pause());
+        }
+
+        public static async Task SetCurrentTimeAsync(this MediaEngine engine, double value, CancellationToken token)
+        {
+            if (engine.IsPaused)
+            {
+                await engine.DoAsync(token, e => e == MediaEngineEvent.TimeUpdate, () => engine.CurrentTime = value);
+            }
+            else
+            {
+                await engine.DoAsync(token, e => e == MediaEngineEvent.Seeked, () => engine.CurrentTime = value);
+            }
+        }
+
+        static async Task DoAsync(this MediaEngine engine, CancellationToken token, Func<MediaEngineEvent, bool> completionEvent, Action action)
+        {
+            var tcs = new TaskCompletionSource<Unit>();
+            token.Register(() => tcs.TrySetCanceled());
+            MediaEngineNotifyDelegate @delegate = (e, p1, p2) =>
+            {
+                Trace.TraceInformation(e.ToString());
+                if (completionEvent(e))
+                    tcs.TrySetResult(Unit.Default);
+                else if (e == MediaEngineEvent.Abort)
+                    tcs.TrySetCanceled();
+                else if (e == MediaEngineEvent.Error)
+                    tcs.TrySetException(new Exception($"An error occured in the pipeline."));
+            };
+            try
+            {
+                engine.PlaybackEvent += @delegate;
+                action();
+                await tcs.Task;
+            }
+            finally
+            {
+                engine.PlaybackEvent -= @delegate;
+            }
+        }
+    }
+}
---
+++ src/VideoPlayer.cs
@@ -0,0 +1,257 @@
+﻿using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.MediaFoundation;
+using SharpDX.WIC;
+using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+using VL.Core;
+using VL.Lib.Basics.Imaging;
+using MapMode = SharpDX.Direct3D11.MapMode;
+using PixelFormat = VL.Lib.Basics.Imaging.PixelFormat;
+
+namespace VL.MediaFoundation
+{
+    // Good source: https://stackoverflow.com/questions/40913196/how-to-properly-use-a-hardware-accelerated-media-foundation-source-reader-to-dec
+    public partial class VideoPlayer : IDisposable
+    {
+        private readonly Subject<IImage> videoFrames = new Subject<IImage>();
+        private (Task, CancellationTokenSource) currentPlayback;
+
+        public VideoPlayer()
+        {
+        }
+
+        public void Update(
+            string url = "http://www.peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+            bool play = false,
+            float rate = 1f,
+            float seekTime = 0f,
+            bool seek = false,
+            float loopStartTime = 0f,
+            float loopEndTime = -1f,
+            bool loop = false,
+            float volume = 1f)
+        {
+            Url = url;
+            Play = play;
+            Rate = rate;
+            SeekTime = seekTime;
+            Seek = seek;
+            LoopStartTime = loopStartTime;
+            LoopEndTime = loopEndTime;
+            Loop = loop;
+            Volume = volume;
+        }
+
+        public IObservable<IImage> Frames => videoFrames;
+
+        public string Url
+        {
+            get => url;
+            set
+            {
+                if (value != url)
+                {
+                    url = value;
+
+                    StopCurrentPlayback();
+
+                    var cts = new CancellationTokenSource();
+                    var task = Task.Run(() => PlayUrl(url, cts.Token));
+                    currentPlayback = (task, cts);
+                }
+            }
+        }
+        string url;
+
+        public bool Play { get; set; } = true;
+
+        public float Rate { get; set; } = 1f;
+
+        public float SeekTime { get; set; }
+
+        public bool Seek { get; set; }
+
+        public float LoopStartTime { get; set; }
+
+        public float LoopEndTime { get; set; } = float.MaxValue;
+
+        public bool Loop { get; set; } = true;
+
+        public float Volume { get; set; } = 1f;
+
+        public float CurrentTime { get; private set; }
+
+        public float Duration { get; private set; }
+
+        async Task PlayUrl(string url, CancellationToken token)
+        {
+            // Reset outputs
+            CurrentTime = default;
+            Duration = default;
+
+            // Initialize MediaFoundation
+            MediaManagerService.Initialize();
+            // Hardware acceleration
+            using var d3dDevice = new Device(DriverType.Hardware, DeviceCreationFlags.BgraSupport | DeviceCreationFlags.VideoSupport);
+            // Add multi thread protection on device (MF is multi-threaded)
+            using var deviceMultithread = d3dDevice.QueryInterface<DeviceMultithread>();
+            deviceMultithread.SetMultithreadProtected(true);
+            // Reset device
+            using var manager = new DXGIDeviceManager();
+            manager.ResetDevice(d3dDevice);
+
+            using var classFactory = new MediaEngineClassFactory();
+            using var mediaEngineAttributes = new MediaEngineAttributes()
+            {
+                // To use WIC disable
+                DxgiManager = manager,
+                VideoOutputFormat = (int)SharpDX.DXGI.Format.B8G8R8A8_UNorm
+            };
+            using var engine = new MediaEngine(classFactory, mediaEngineAttributes);
+            using var engineEx = engine.QueryInterface<MediaEngineEx>();
+
+            // Wait for MediaEngine to be ready
+            await engine.LoadAsync(url, token);
+
+            engine.GetNativeVideoSize(out var width, out var height);
+            Duration = (float)engine.Duration;
+
+            //var fac = new ImagingFactory();
+            //using var bitmap = new Bitmap(fac, width, height, SharpDX.WIC.PixelFormat.Format32bppBGRA, BitmapCreateCacheOption.CacheOnLoad);
+            var textureDesc = new Texture2DDescription()
+            {
+                Width = width,
+                Height = height,
+                MipLevels = 0,
+                ArraySize = 1,
+                Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
+                SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+                Usage = ResourceUsage.Staging,
+                BindFlags = BindFlags.None,
+                CpuAccessFlags = CpuAccessFlags.Read,
+                OptionFlags = ResourceOptionFlags.None
+            };
+            using var dstTexture = new Texture2D(d3dDevice, textureDesc);
+
+            textureDesc.Usage = ResourceUsage.Default;
+            textureDesc.BindFlags = BindFlags.RenderTarget;
+            textureDesc.CpuAccessFlags = CpuAccessFlags.None;
+
+            using var renderTexture = new Texture2D(d3dDevice, textureDesc);
+
+            var info = new ImageInfo(width, height, PixelFormat.B8G8R8A8);
+
+            while (!token.IsCancellationRequested)
+            {
+                if (Loop != engine.Loop)
+                    engine.Loop = Loop;
+
+                if (Rate != engine.PlaybackRate)
+                {
+                    engine.PlaybackRate = Rate;
+                    engine.DefaultPlaybackRate = Rate;
+                }
+
+                var volume = VLMath.Clamp(Volume, 0f, 1f);
+                if (volume != engine.Volume)
+                    engine.Volume = volume;
+
+                if (Seek)
+                {
+                    var seekTime = VLMath.Clamp(SeekTime, 0, Duration);
+                    await engine.SetCurrentTimeAsync(seekTime, token);
+                }
+
+                // Check playing state
+                if (Play)
+                {
+                    if (engine.IsPaused)
+                        await engine.PlayAsync(token);
+                }
+                else
+                {
+                    if (!engine.IsPaused)
+                        await engine.PauseAsync(token);
+                    else
+                        await Task.Delay(10);
+
+                    continue;
+                }
+
+                if (engine.OnVideoStreamTick(out var presentationTimeTicks))
+                {
+                    var currentTime = CurrentTime = (float)TimeSpan.FromTicks(presentationTimeTicks).TotalSeconds;
+
+                    if (Loop || presentationTimeTicks < 0)
+                    {
+                        var loopStartTime = VLMath.Clamp(LoopStartTime, 0f, Duration);
+                        var loopEndTime = VLMath.Clamp(LoopEndTime < 0 ? float.MaxValue : LoopEndTime, 0f, Duration);
+                        if (currentTime < loopStartTime || currentTime > loopEndTime)
+                        {
+                            if (Rate >= 0)
+                                await engine.SetCurrentTimeAsync(loopStartTime, token);
+                            else
+                                await engine.SetCurrentTimeAsync(loopEndTime, token);
+
+                            continue;
+                        }
+                    }
+
+                    //engine.TransferVideoFrame(bitmap, default, new SharpDX.Mathematics.Interop.RawRectangle(0, 0, width, height), default);
+                    //using var bitmapLock = bitmap.Lock(BitmapLockFlags.Read);
+                    //var data = bitmapLock.Data;
+                    //using var image = new IntPtrImage(data.DataPointer, data.Pitch * height, info);
+
+                    engine.TransferVideoFrame(renderTexture, default, new SharpDX.Mathematics.Interop.RawRectangle(0, 0, width, height), default);
+
+                    var deviceContext = d3dDevice.ImmediateContext;
+                    deviceContext.CopyResource(renderTexture, dstTexture);
+                    //deviceContext.Flush();
+
+                    var data = deviceContext.MapSubresource(dstTexture, 0, MapMode.Read, MapFlags.None);
+                    try
+                    {
+                        using var image = new IntPtrImage(data.DataPointer, data.RowPitch * height, info);
+                        videoFrames.OnNext(image);
+                    }
+                    finally
+                    {
+                        deviceContext.UnmapSubresource(dstTexture, 0);
+                    }
+                }
+            }
+
+            engine.Shutdown();
+        }
+
+        void StopCurrentPlayback()
+        {
+            var (currentTask, currentCts) = currentPlayback;
+            if (currentTask != null)
+            {
+                currentCts.Cancel();
+                try
+                {
+                    currentTask.Wait();
+                }
+                catch (Exception e)
+                {
+                    Trace.TraceError(e.ToString());
+                }
+                currentCts.Dispose();
+            }
+            currentPlayback = default;
+        }
+
+        public void Dispose()
+        {
+            StopCurrentPlayback();
+            videoFrames.Dispose();
+        }
+    }
+}
