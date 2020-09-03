f307ab36beff5f15dd3ff5584b03bdd09a08e42c
Elias Holzer <elias@vvvv.org>
2020-09-03 17:16:25 +0200

    wip

---
+++ src/ColorSpaceConverter.cs
@@ -0,0 +1,50 @@
+﻿using Stride.Graphics;
+using Stride.Rendering;
+using System;
+
+namespace VL.MediaFoundation
+{
+    sealed class ColorSpaceConverter : IDisposable
+    {
+        private readonly RenderDrawContext renderContext;
+        private Texture srgbTexture;
+
+        public ColorSpaceConverter(RenderDrawContext renderContext)
+        {
+            this.renderContext = renderContext;
+        }
+
+        public void Dispose()
+        {
+            srgbTexture?.Dispose();
+        }
+
+        public Texture ToDeviceColorSpace(Texture texture)
+        {
+            var graphicsDevice = renderContext.GraphicsDevice;
+
+            // Check if conversion is needed
+            if (graphicsDevice.ColorSpace == ColorSpace.Gamma)
+                return texture;
+
+            var desc = texture.Description;
+            if (desc.Format.IsSRgb() || !desc.Format.HasSRgbEquivalent())
+                return texture;
+
+            desc.Format = desc.Format.ToSRgb();
+            desc.Flags = TextureFlags.ShaderResource;
+
+            // Ensure sRGB texture has required format and dimension
+            if (srgbTexture is null || desc != srgbTexture.Description)
+            {
+                srgbTexture?.Dispose();
+                srgbTexture = Texture.New(graphicsDevice, desc);
+            }
+
+            // Copy the texture
+            renderContext.CommandList.Copy(texture, srgbTexture);
+
+            return srgbTexture;
+        }
+    }
+}
--- src/VideoPlayer.cs
+++ src/VideoPlayer.cs
@@ -1,29 +1,203 @@
 ﻿using SharpDX.Direct3D;
 using SharpDX.Direct3D11;
+using SharpDX.Mathematics.Interop;
 using SharpDX.MediaFoundation;
+using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Graphics;
+using Stride.Rendering;
 using System;
 using System.Diagnostics;
-using System.Reactive.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using VL.Core;
-using VL.Lib.Basics.Imaging;
 using VL.Lib.Basics.Resources;
+using VL.Stride;
+using PixelFormat = Stride.Graphics.PixelFormat;
 
 namespace VL.MediaFoundation
 {
     // Good source: https://stackoverflow.com/questions/40913196/how-to-properly-use-a-hardware-accelerated-media-foundation-source-reader-to-dec
-    public partial class VideoPlayer
+    public partial class VideoPlayer : IDisposable
     {
-        readonly IResourceProvider<Device> deviceProvider;
+        private readonly IResourceHandle<RenderDrawContext> renderDrawContextHandle;
+        private readonly ColorSpaceConverter colorSpaceConverter;
+        private MediaEngine engine;
+        private bool invalidated;
 
         public VideoPlayer(NodeContext nodeContext)
         {
-            // Our assembly initializer ensures that a device provider is registered
-            deviceProvider = nodeContext.Factory.CreateService<IResourceProvider<Device>>(nodeContext);
+            renderDrawContextHandle = nodeContext.GetGameProvider()
+                .Bind(g => RenderContext.GetShared(g.Services).GetThreadContext())
+                .GetHandle() ?? throw new ServiceNotFoundException(typeof(IResourceProvider<Game>));
+
+            colorSpaceConverter = new ColorSpaceConverter(renderDrawContextHandle.Resource);
+
+            // Initialize MediaFoundation
+            MediaManagerService.Initialize();
+
+            using var mediaEngineAttributes = new MediaEngineAttributes()
+            {
+                // _SRGB doesn't work :/ Getting invalid argument exception later in TransferVideoFrame
+                VideoOutputFormat = (int)SharpDX.DXGI.Format.B8G8R8A8_UNorm
+            };
+
+            var graphicsDevice = renderDrawContextHandle.Resource.GraphicsDevice;
+            var device = SharpDXInterop.GetNativeDevice(graphicsDevice) as Device;
+            if (device != null)
+            {
+                // Add multi thread protection on device (MF is multi-threaded)
+                using var deviceMultithread = device.QueryInterface<DeviceMultithread>();
+                deviceMultithread.SetMultithreadProtected(true);
+
+
+                // Reset device
+                using var manager = new DXGIDeviceManager();
+                manager.ResetDevice(device);
+                mediaEngineAttributes.DxgiManager = manager;
+            }
+
+            using var classFactory = new MediaEngineClassFactory();
+            engine = new MediaEngine(classFactory, mediaEngineAttributes);
+            engine.PlaybackEvent += Engine_PlaybackEvent;
         }
 
-        public void Update(
+        private void Engine_PlaybackEvent(MediaEngineEvent mediaEvent, long param1, int param2)
+        {
+            Trace.TraceInformation(mediaEvent.ToString());
+            switch (mediaEvent)
+            {
+                case MediaEngineEvent.LoadedMetadata:
+                    invalidated = true;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// The URL of the media to play.
+        /// </summary>
+        public string Url
+        {
+            set
+            {
+                if (value != url)
+                {
+                    url = value;
+                    engine.Source = value;
+                }
+            }
+        }
+        string url;
+
+        /// <summary>
+        /// Set to true to start playback, false to pause playback.
+        /// </summary>
+        public bool Play
+        {
+            set
+            {
+                if (value != play)
+                {
+                    play = value;
+
+                    if (value)
+                        engine.Play();
+                    else
+                        engine.Pause();
+                }
+            }
+        }
+        bool play;
+
+        /// <summary>
+        /// Gets or sets the rate at which the media is being played back.
+        /// </summary>
+        public float Rate
+        {
+            get => (float)engine.PlaybackRate;
+            set => engine.PlaybackRate = value;
+        }
+
+        public float SeekTime { get; set; }
+
+        public bool Seek { get; set; }
+
+        public float LoopStartTime { get; set; }
+
+        public float LoopEndTime { get; set; } = float.MaxValue;
+
+        public bool Loop
+        {
+            get => engine.Loop;
+            set => engine.Loop = value;
+        }
+
+        /// <summary>
+        /// The audio volume.
+        /// </summary>
+        public float Volume
+        {
+            get => (float)engine.Volume;
+            set => engine.Volume = VLMath.Clamp(value, 0f, 1f);
+        }
+
+        /// <summary>
+        /// The normalized source rectangle.
+        /// </summary>
+        public RectangleF? SourceBounds { private get; set; }
+
+        /// <summary>
+        /// The border color.
+        /// </summary>
+        public Color4? BorderColor { private get; set; }
+
+        /// <summary>
+        /// The size of the output texture. Use zero to take the size from the video.
+        /// </summary>
+        public Size2 TextureSize
+        {
+            set
+            {
+                if (value != textureSize)
+                {
+                    textureSize = value;
+                    invalidated = true;
+                }
+            }
+        }
+        Size2 textureSize;
+
+        /// <summary>
+        /// Whether or not playback started.
+        /// </summary>
+        public bool Playing => !engine.IsPaused;
+
+        /// <summary>
+        /// A Boolean which is true if the media contained in the element has finished playing.
+        /// </summary>
+        public bool IsEnded => engine.IsEnded;
+
+        /// <summary>
+        /// The current playback time in seconds
+        /// </summary>
+        public float CurrentTime => (float)engine.CurrentTime;
+
+        /// <summary>
+        /// The length of the element's media in seconds.
+        /// </summary>
+        public float Duration => (float)engine.Duration;
+
+        /// <summary>
+        /// The current state of the fetching of media over the network.
+        /// </summary>
+        public NetworkState NetworkState => (NetworkState)engine.NetworkState;
+
+        /// <summary>
+        /// The readiness state of the media.
+        /// </summary>
+        public ReadyState ReadyState => (ReadyState)engine.ReadyState;
+
+        // This method is not really needed but makes it simpler to work with inside VL
+        public Texture Update(
             string url = "http://www.peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
             bool play = false,
             float rate = 1f,
@@ -32,7 +206,11 @@ namespace VL.MediaFoundation
             float loopStartTime = 0f,
             float loopEndTime = -1f,
             bool loop = false,
-            float volume = 1f)
+            float volume = 1f,
+            int textureWidth = 0,
+            int textureHeight = 0,
+            RectangleF? sourceBounds = default,
+            Color4? borderColor = default)
         {
             Url = url;
             Play = play;
@@ -43,144 +221,33 @@ namespace VL.MediaFoundation
             LoopEndTime = loopEndTime;
             Loop = loop;
             Volume = volume;
+            TextureSize = new Size2(textureWidth, textureHeight);
+            SourceBounds = sourceBounds;
+            BorderColor = borderColor;
+            Update();
+            return currentVideoFrame;
         }
 
-        public IObservable<Texture2D> Frames { get; private set; } = Observable.Empty<Texture2D>();
-
-        public string Url
+        void Update()
         {
-            get => url;
-            set
+            if (ReadyState <= ReadyState.HaveNothing)
             {
-                if (value != url)
-                {
-                    url = value;
-
-                    Frames = Observable.Create<Texture2D>((observer, token) => PlayUrl(Url, observer, token)).Publish().RefCount();
-                }
+                currentVideoFrame?.Dispose();
+                currentVideoFrame = null;
+                return;
             }
-        }
-        string url;
 
-        public bool Play { get; set; } = true;
-
-        public float Rate { get; set; } = 1f;
-
-        public float SeekTime { get; set; }
-
-        public bool Seek { get; set; }
-
-        public float LoopStartTime { get; set; }
-
-        public float LoopEndTime { get; set; } = float.MaxValue;
-
-        public bool Loop { get; set; } = true;
-
-        public float Volume { get; set; } = 1f;
-
-        public float CurrentTime { get; private set; }
-
-        public float Duration { get; private set; }
-
-        async Task PlayUrl(string url, IObserver<Texture2D> observer, CancellationToken token)
-        {
-            using var deviceHandle = deviceProvider.GetHandle();
-            var device = deviceHandle.Resource;
-
-            // Add multi thread protection on device (MF is multi-threaded)
-            using var deviceMultithread = device.QueryInterface<DeviceMultithread>();
-            deviceMultithread.SetMultithreadProtected(true);
-
-            // Initialize MediaFoundation
-            MediaManagerService.Initialize();
-
-            // Reset device
-            using var manager = new DXGIDeviceManager();
-            manager.ResetDevice(device);
-
-            using var classFactory = new MediaEngineClassFactory();
-            using var mediaEngineAttributes = new MediaEngineAttributes()
+            if (ReadyState >= ReadyState.HaveMetadata)
             {
-                // To use WIC disable
-                DxgiManager = manager,
-                VideoOutputFormat = (int)SharpDX.DXGI.Format.B8G8R8A8_UNorm
-            };
-            using var engine = new MediaEngine(classFactory, mediaEngineAttributes);
-
-            MediaEngineNotifyDelegate logger = (e, p1, p2) => Trace.TraceInformation(e.ToString());
-            engine.PlaybackEvent += logger;
-            try
-            {
-                // Run the playback on a background thread
-                await Task.Run(() => PlayUrl(device, engine, url, observer, token));
-            }
-            finally
-            {
-                engine.PlaybackEvent -= logger;
-                engine.Shutdown();
-            }
-        }
-
-        async Task PlayUrl(Device device, MediaEngine engine, string url, IObserver<Texture2D> observer, CancellationToken token)
-        {
-            // Reset outputs
-            CurrentTime = default;
-            Duration = default;
-
-            // Wait for MediaEngine to be ready
-            await engine.LoadAsync(url, token);
-
-            engine.GetNativeVideoSize(out var width, out var height);
-            Duration = (float)engine.Duration;
-
-            //var fac = new ImagingFactory();
-            //using var bitmap = new Bitmap(fac, width, height, SharpDX.WIC.PixelFormat.Format32bppBGRA, BitmapCreateCacheOption.CacheOnLoad);
-            // and later
-            //engine.TransferVideoFrame(bitmap, default, new SharpDX.Mathematics.Interop.RawRectangle(0, 0, width, height), default);
-            //using var bitmapLock = bitmap.Lock(BitmapLockFlags.Read);
-            //var data = bitmapLock.Data;
-            //using var image = new IntPtrImage(data.DataPointer, data.Pitch * height, info);
-
-            var textureDesc = new Texture2DDescription()
-            {
-                Width = width,
-                Height = height,
-                MipLevels = 1,
-                ArraySize = 1,
-                Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
-                SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
-                Usage = ResourceUsage.Default,
-                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
-                CpuAccessFlags = CpuAccessFlags.None,
-                OptionFlags = ResourceOptionFlags.None
-            };
-
-            using var outputTexture = new Texture2D(device, textureDesc);
-
-            while (!token.IsCancellationRequested)
-            {
-                if (Loop != engine.Loop)
-                    engine.Loop = Loop;
-
-                if (Rate != engine.PlaybackRate)
-                {
-                    engine.PlaybackRate = Rate;
-                    engine.DefaultPlaybackRate = Rate;
-                }
-
-                var volume = VLMath.Clamp(Volume, 0f, 1f);
-                if (volume != engine.Volume)
-                    engine.Volume = volume;
-
                 if (Seek)
                 {
                     var seekTime = VLMath.Clamp(SeekTime, 0, Duration);
                     engine.CurrentTime = seekTime;
                 }
 
-                var currentTime = CurrentTime = (float)engine.CurrentTime;
                 if (Loop)
                 {
+                    var currentTime = CurrentTime;
                     var loopStartTime = VLMath.Clamp(LoopStartTime, 0f, Duration);
                     var loopEndTime = VLMath.Clamp(LoopEndTime < 0 ? float.MaxValue : LoopEndTime, 0f, Duration);
                     if (currentTime < loopStartTime || currentTime > loopEndTime)
@@ -189,33 +256,126 @@ namespace VL.MediaFoundation
                             engine.CurrentTime = loopStartTime;
                         else
                             engine.CurrentTime = loopEndTime;
-
-                        continue;
                     }
                 }
+            }
 
-                // Check playing state
-                if (Play)
+            if (ReadyState >= ReadyState.HaveCurrentData && engine.OnVideoStreamTick(out var presentationTimeTicks))
+            {
+                if (invalidated || currentVideoFrame is null)
                 {
-                    if (engine.IsPaused)
-                        engine.Play();
+                    invalidated = false;
+
+                    renderTarget?.Dispose();
+
+                    engine.GetNativeVideoSize(out var width, out var height);
+
+                    // Apply user specified size
+                    var x = textureSize;
+                    if (x.Width > 0)
+                        width = x.Width;
+                    if (x.Height > 0)
+                        height = x.Height;
+
+                    var graphicsDevice = renderDrawContextHandle.Resource.GraphicsDevice;
+
+                    // _SRGB doesn't work :/ Getting invalid argument exception in TransferVideoFrame
+                    renderTarget = Texture.New2D(graphicsDevice, width, height, PixelFormat.B8G8R8A8_UNorm, TextureFlags.RenderTarget | TextureFlags.ShaderResource);
                 }
-                else
+
+                if (SharpDXInterop.GetNativeResource(renderTarget) is Texture2D nativeRenderTarget)
                 {
-                    if (!engine.IsPaused)
-                        engine.Pause();
+                    engine.TransferVideoFrame(
+                        nativeRenderTarget, 
+                        ToVideoRect(SourceBounds), 
+                        new RawRectangle(0, 0, renderTarget.ViewWidth, renderTarget.ViewHeight), 
+                        ToRawColorBGRA(BorderColor));
+
+                    // Apply color space conversion if necessary
+                    currentVideoFrame = colorSpaceConverter.ToDeviceColorSpace(renderTarget);
                 }
-
-                // It's imperative to call this function in a loop. Otherwise the pipeline might get stuck.
-                if (engine.OnVideoStreamTick(out var presentationTimeTicks))
-                {
-                    engine.TransferVideoFrame(outputTexture, default, new SharpDX.Mathematics.Interop.RawRectangle(0, 0, width, height), default);
-
-                    observer.OnNext(outputTexture);
-                }
-
-                await Task.Delay(10);
             }
         }
+        Texture renderTarget;
+        Texture currentVideoFrame;
+
+        static VideoNormalizedRect? ToVideoRect(RectangleF? rect)
+        {
+            if (rect.HasValue)
+            {
+                var x = rect.Value;
+                return new VideoNormalizedRect() 
+                { 
+                    Left = VLMath.Clamp(x.Left, 0f, 1f), 
+                    Bottom = VLMath.Clamp(x.Bottom, 0f, 1f),
+                    Right = VLMath.Clamp(x.Right, 0f, 1f), 
+                    Top = VLMath.Clamp(x.Top, 0f, 1f)
+                };
+            }
+            return default;
+        }
+
+        static RawColorBGRA? ToRawColorBGRA(Color4? color)
+        {
+            if (color.HasValue)
+            {
+                color.Value.ToBgra(out var r, out var g, out var b, out var a);
+                return new RawColorBGRA(b, g, r, a);
+            }
+            return default;
+        }
+
+        public void Dispose()
+        {
+            engine.PlaybackEvent -= Engine_PlaybackEvent;
+            engine.Shutdown();
+            engine.Dispose();
+            colorSpaceConverter.Dispose();
+            renderTarget?.Dispose();
+        }
+    }
+
+    public enum NetworkState : short
+    {
+        /// <summary>
+        /// There is no data yet. Also, readyState is HaveNothing.
+        /// </summary>
+        Empty,
+        /// <summary>
+        /// HTMLMediaElement is active and has selected a resource, but is not using the network.
+        /// </summary>
+        Idle,
+        /// <summary>
+        /// The browser is downloading HTMLMediaElement data.
+        /// </summary>
+        Loading,
+        /// <summary>
+        /// No HTMLMediaElement src found.
+        /// </summary>
+        NoSource
+    }
+
+    public enum ReadyState : short
+    {
+        /// <summary>
+        /// No information is available about the media resource.
+        /// </summary>
+        HaveNothing,
+        /// <summary>
+        /// Enough of the media resource has been retrieved that the metadata attributes are initialized. Seeking will no longer raise an exception.
+        /// </summary>
+        HaveMetadata,
+        /// <summary>
+        /// Data is available for the current playback position, but not enough to actually play more than one frame.
+        /// </summary>
+        HaveCurrentData,
+        /// <summary>
+        /// Data for the current playback position as well as for at least a little bit of time into the future is available (in other words, at least two frames of video, for example).
+        /// </summary>
+        HaveFutureData,
+        /// <summary>
+        /// Enough data is available—and the download rate is high enough—that the media can be played through to the end without interruption.
+        /// </summary>
+        HaveEnoughData
     }
 }
