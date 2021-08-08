1d15422614abc4e4653251b59517f8b11b4038a9
Elias Holzer <elias@vvvv.org>
2021-08-08 12:58:22 +0200

    WIP - reorganizing the projects/packages to separate between Stride and Skia rendering backends and dependencies getting pulled in on export

---
+++ src/VL.Video.MediaFoundation/Initialization.cs
@@ -0,0 +1,61 @@
+﻿using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using System.Collections.Immutable;
+using System.Linq;
+using VL.Core;
+using VL.Core.CompilerServices;
+using VL.Lib.Basics.Resources;
+
+[assembly: AssemblyInitializer(typeof(VL.MediaFoundation.Initialization))]
+
+namespace VL.MediaFoundation
+{
+    public sealed class Initialization : AssemblyInitializer<Initialization>
+    {
+        protected override void RegisterServices(IVLFactory factory)
+        {
+            if (!factory.HasService<NodeContext, IResourceProvider<Device>>())
+            {
+                factory.RegisterService<NodeContext, IResourceProvider<Device>>(nodeContext =>
+                {
+                    // One per entry point
+                    return ResourceProvider.NewPooledPerApp(nodeContext, () => new Device(DriverType.Hardware, DeviceCreationFlags.BgraSupport | DeviceCreationFlags.VideoSupport));
+                });
+            }
+            factory.RegisterNodeFactory("VL.Video.MediaFoundation.ControlNodes", f =>
+            {
+                // TODO: fragmented = true causes troubles on disconnect, apparently the default value injection can't deal with nullables -> injects 0 instead of null
+                var nodes = ImmutableArray.Create(
+                    f.NewNodeDescription(nameof(CameraControls), "Video.MediaFoundation.Advanced", fragmented: false, bc =>
+                    {
+                        return bc.NewNode(
+                            inputs: CameraControls.Default.Properties.Select(p => Pin(bc, p.Name.ToString())),
+                            outputs: new[] { bc.Pin("Output", typeof(CameraControls)) },
+                            newNode: ibc =>
+                            {
+                                var controls = new CameraControls();
+                                return ibc.Node(
+                                    inputs: controls.Properties,
+                                    outputs: new[] { ibc.Output(() => controls) });
+                            });
+                    }),
+                    f.NewNodeDescription(nameof(VideoControls), "Video.MediaFoundation.Advanced", fragmented: false, bc =>
+                    {
+                        return bc.NewNode(
+                            inputs: VideoControls.Default.Properties.Select(p => Pin(bc, p.Name.ToString())),
+                            outputs: new[] { bc.Pin("Output", typeof(VideoControls)) },
+                            newNode: ibc =>
+                            {
+                                var controls = new VideoControls();
+                                return ibc.Node(
+                                    inputs: controls.Properties,
+                                    outputs: new[] { ibc.Output(() => controls) });
+                            });
+                    }));
+                return NodeBuilding.NewFactoryImpl(nodes);
+            });
+        }
+
+        static IVLPinDescription Pin(NodeBuilding.NodeDescriptionBuildContext bc, string name) => bc.Pin(name, typeof(Optional<float>));
+    }
+}
---
+++ src/VL.Video.MediaFoundation/MediaEngineExtensions.cs
@@ -0,0 +1,41 @@
+﻿using SharpDX.MediaFoundation;
+using System;
+using System.Reactive;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VL.MediaFoundation
+{
+    static class MediaEngineExtensions
+    {
+        public static async Task LoadAsync(this MediaEngine engine, string url, CancellationToken token)
+        {
+            await engine.DoAsync(token, () => engine.Source = url, e => e == MediaEngineEvent.LoadedMetadata);
+        }
+
+        static async Task DoAsync(this MediaEngine engine, CancellationToken token, Action action, Func<MediaEngineEvent, bool> completionEvent)
+        {
+            var tcs = new TaskCompletionSource<Unit>();
+            token.Register(() => tcs.TrySetCanceled());
+            MediaEngineNotifyDelegate @delegate = (e, p1, p2) =>
+            {
+                if (completionEvent(e))
+                    tcs.TrySetResult(Unit.Default);
+                else if (e == MediaEngineEvent.Abort)
+                    tcs.TrySetCanceled();
+                else if (e == MediaEngineEvent.Error)
+                    tcs.TrySetException(new Exception($"An error occured in the pipeline."));
+            };
+            try
+            {
+                engine.PlaybackEvent += @delegate;
+                action();
+                await tcs.Task;
+            }
+            finally
+            {
+                engine.PlaybackEvent -= @delegate;
+            }
+        }
+    }
+}
